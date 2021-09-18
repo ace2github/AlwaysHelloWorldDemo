97c85e8349e725cf02edbe985f33eea86dc1a602
ChaohuiChen <173141667@qq.com>
2021-09-18 17:49:11 +0800

    mp

---
+++ MP/HelloWorldDemo/pages/detailbase/detailbase.vue
@@ -0,0 +1,118 @@
+<template>
+	<view class="content">
+		<view class="top-header">
+			<view class="body-top"></view>
+		</view>
+		
+		<view class="body-main">
+			<view class="desc-content">
+				<icon :type="info" size="26"/>
+				<text>{{title}}</text>
+			</view>
+			<view class="body-content">
+				<uniTable border stripe emptyText="暂无数据">
+					<uniTr v-for="(list,index) in tblDataList" :key="obj.date">
+						<uniTd v-for="(value,v_index) in list" >{{value}}</uniTd>
+					</uniTr>
+				</uniTable>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	import {uniTable} from "@/uni_modules/uni-table/components/uni-table/uni-table";
+	import {uniTr} from "@/uni_modules/uni-table/components/uni-tr/uni-tr";
+	import {uniTd} from "@/uni_modules/uni-table/components/uni-td/uni-td";
+	import {uniTh} from "@/uni_modules/uni-table/components/uni-th/uni-th";
+	export default {
+		components:{
+			uniTable,uniTr,uniTd,uniTh
+		},
+		data() {
+			return {
+				title: "现行的LPR包括1年期和5年期以上两个品种。\n",
+				tblDataList: reqeustLprDataList(),
+			}
+		},
+		methods: {
+			
+		}
+	}
+	
+	function reqeustLprDataList() {
+		var list = 
+		[
+			["日期",  "1年期",  "5年期以上"],
+			["2021-08-20",  "3.85",  "4.65"],
+			["2021-07-20",  "3.85",  "4.65"],
+			["2021-06-20",  "3.85",  "4.65"],
+			["2021-05-20",  "3.85",  "4.65"],
+			["2021-04-20",  "3.85",  "4.65"],
+			["2021-03-20",  "3.85",  "4.65"],
+		]
+		
+		list =
+		[
+			["日期",  "1年期",  "5年期以上"],
+			["2021-08-20",  "3.85",  "4.65"],
+			["2021-07-20",  "3.85",  "4.65"],
+			["2021-06-20",  "3.85",  "4.65"],
+			["2021-05-20",  "3.85",  "4.65"],
+			["2021-04-20",  "3.85",  "4.65"],
+			["2021-03-20",  "3.85",  "4.65"],
+		]
+		
+		return list;
+	}
+</script>
+
+<style>
+	.content {
+		width: 100%;
+		height: 100%;
+	}
+	// header
+	.top-header {
+		width: 100%;
+		height: 50rpx;
+		background-color: #09D0B2;
+		
+		display: flex;
+		flex-direction: column;
+		justify-content: flex-end;
+	}
+	.body-top {
+		width: 100%;
+		height: 30rpx;
+		border-top-right-radius: 30rpx;
+		border-top-left-radius: 30rpx;
+		background-color: #fff;
+	}
+	
+	// body
+	.body-main {
+		margin: 20rpx 15rpx 0rpx 15rpx;
+		display: flex;
+		flex-direction: column;
+		justify-content: space-between;
+		align-items: stretch;
+		flex-wrap: wrap;
+	}
+	
+	.desc-content {
+		display: flex;
+		flex-direction: row;
+		justify-content: flex-start;
+	}
+	.desc-content text {
+		font-size: 26rpx;
+		line-height: 26rpx;
+	}
+	
+	.body-content {
+		height: 500rpx;
+		width: 100%;
+		margin-top: 40rpx;
+	}
+</style>
---
+++ MP/HelloWorldDemo/pages/index/index.vue
@@ -0,0 +1,130 @@
+<template>
+	<view class="content">
+		<view class="header">
+			<view class="header_top"></view>
+			<view class="banner">
+				<image class="banner_image" src="https://pic.quanjing.com/ux/58/QJ6551561157.jpg?x-oss-process=style/794ws" mode="aspectFill"></image>
+			</view>
+		</view>
+		
+		<view class="body-area">
+			<view class="item-cell" v-for="(obj,index) in dataList">
+				<view class="cell-content" @click="cellClickhandler(index)">
+					<view class="module-c">
+						<text>{{obj.title}}</text>
+					</view>
+					<view class="cell-main">
+						<text>{{obj.desc}}</text>
+					</view>
+				</view>
+				<view style="height: 1px; background-color: #efefef;"></view>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello',
+				dataList: [
+					{"title": "LPR", "desc": "desc \nxxx"},
+					{"title": "CPI", "desc": "desc xxx"},
+					{"title": "油价", "desc": "desc xxx"},
+					{"title": "xxx", "desc": "desc xxx"},
+					{"title": "xxx", "desc": "desc xxx"},
+				]
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+			cellClickhandler(index){
+				console.log('cell click', index)
+				uni.navigateTo({
+					url:"../detailbase/detailbase"
+				})
+			}
+		}
+	}
+</script>
+
+<style>
+	.content {
+		height: 100%;
+		width: 100%;
+	}
+	
+	.header {
+		height: 240;
+		width: 100%;
+	}
+	
+	.header_top {
+		height: 80rpx;
+		width: 100%;
+		background-color: #09D0B2;
+	}
+	
+	// banner
+	.banner {
+		height: 220rpx;
+		margin: -60rpx 10rpx 0rpx 10rpx;
+	}
+	
+	.banner image {
+		height: 100%;
+		width: 100%;
+		border-radius: 10rpx;
+	}
+	
+	
+	// body
+	.body-area {
+		margin: 20rpx 10rpx 0rpx 10rpx;
+		display: flex;
+		flex-direction: column;
+		justify-content: space-between;
+		align-items: stretch;
+		flex-wrap: wrap;
+	}
+	
+
+	// cell
+	.item-cell {
+		height: 120rpx;
+	}
+	.cell-content {
+		height: 119rpx;
+		
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+	}
+	.module-c {
+		width: 100rpx;
+		height: 100rpx;
+		border-radius: 50rpx;
+		background-color: #09D0B2;
+		display: flex;
+		flex-direction: row;
+		justify-content: center;
+		align-items: center;
+	}
+	.module-c text {
+		font-size: 30rpx;
+		line-height: 30rpx;
+		color: #FFFFFF;
+	}
+	
+	.cell-main {
+		margin-left: 20rpx;
+	}
+	.cell-main text {
+		font-size: 26rpx;
+		line-height: 26rpx;
+		color: #000;
+	}
+</style>
